c084661e81891339adfc8b652f1c8245a8a6670b
suntengfei <15102377525@163.com>
2016-06-30 08:48:21 +0800

    添加空格

--- stk_bike/src/main/java/com/stk/controller/business/bikefunctionmanage/LowpowerController.java
+++ stk_bike/src/main/java/com/stk/controller/business/bikefunctionmanage/LowpowerController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.stk.controller.base.BaseController;
+
 @Controller
-@RequestMapping(value="/lowpower/")
-//低电量车辆管理
-public class LowpowerController extends BaseController{
-	@RequestMapping(value="select")
-	public ModelAndView select(){
-		ModelAndView mv=new ModelAndView();
+@RequestMapping(value = "/lowpower/")
+// 低电量车辆管理
+public class LowpowerController extends BaseController {
+	@RequestMapping(value = "select")
+	public ModelAndView select() {
+		ModelAndView mv = new ModelAndView();
+		
 		mv.setViewName("business/bikefunctionmanage/lowpowerManager");
 		return mv;
 	}
